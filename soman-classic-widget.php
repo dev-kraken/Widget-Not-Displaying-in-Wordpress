a4e18ef220a4566725d911d6fa95aaf67fefcb13
Soman Bandesha <86368498+somanbandesha@users.noreply.github.com>
2021-07-23 23:25:26 +0400

    Create soman-classic-widget.php

---
+++ soman-classic-widget.php
@@ -0,0 +1,7 @@
+
+// Soman Classic Widgets
+// By Soman Bandesha
+function example_theme_support() {
+    remove_theme_support( 'widgets-block-editor' );
+}
+add_action( 'after_setup_theme', 'example_theme_support' );
